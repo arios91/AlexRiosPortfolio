ebe680b8cc55000b06a2b9c0b6b443aacff90ea5
Alejandro Rios <Alx.Rios91@gmail.com>
2019-08-14 23:59:30 -0500

    added flowershop to projects

--- src/components/layout/Projects.js
+++ src/components/layout/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {pafs} from '../../assets/images/petalosArteScreenshot.png';
 
 class Projects extends Component {
     render() {
@@ -6,6 +7,30 @@ class Projects extends Component {
             <div id="workSection" className="text-center">
                 <div className="border-bottom">
                     <h2>Projects</h2>
+                </div>
+                <div className="container">
+                    <div className="row my-2">
+                        <div className="col-6 p-0 text-left">
+                            <img src="https://i.imgur.com/yAL7idq.png" alt="test" width="100%"/>
+                        </div>
+                        <div className="col-6 text-center">
+                            <a href="https://www.petalosarte.com" target="_blank" className="project-title">
+                                Petalos y Arte Flower Shop
+                            </a>
+                            <p className="project-description">
+                                Fully functional e-commerce site custom built for a local flower shop<br/>
+                                which provides same day fresh flower deliver for South Texas.<br/>
+                                Built with Angular on a Node.js backend;
+                            </p>
+                            <div className="w-100">
+                                <p className="project-stack">
+                                    Tech Stack:<br/>
+                                    <p>Angular, Node.js</p> 
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
