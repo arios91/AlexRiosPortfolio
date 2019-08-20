c4e725c63b11da8db234383d98feffc0655a1dfe
Alejandro Rios <Alx.Rios91@gmail.com>
2019-08-19 23:23:36 -0500

    fixed project ordering on mobile

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Spring} from 'react-spring/renderprops';
+import './Projects.css';
 
 class Projects extends Component {
     render() {
@@ -16,40 +17,30 @@ class Projects extends Component {
                         </div>
                         <div className="container">
                             <div className="row my-2">
-                                <div className="col-6 p-0 text-left">
-                                    <img src="https://i.imgur.com/yAL7idq.png" alt="test" width="100%"/>
+                                <div className="col-12 col-lg-6 p-0">
+                                    <img src="https://i.imgur.com/yAL7idq.png" alt="flower shop site image" width="100%"/>
                                 </div>
-                                <div className="col-6 text-center">
+                                <div className="col-12 col-lg-6 text-center">
                                     <a href="https://www.petalosarte.com" target="_blank" className="project-title">
                                         Petalos y Arte Flower Shop
                                     </a>
                                     <p className="project-description">
                                         Fully functional e-commerce site custom built for a local flower shop<br/>
                                         which provides same day fresh flower delivery for South Texas.<br/>
-                                        Built with Angular on a Node.js backend;
+                                        Built with Angular on a Node.js backend.
                                     </p>
-                                    <div className="w-100">
-                                        <p className="project-stack">
-                                            Tech Stack:<br/>
-                                            Angular, Node.js 
-                                        </p>
-
-                                    </div>
                                 </div>
-                                <div className="col-6 text-cent">
+                            </div>
+                            <div className="row my-2">
+                                <div className="col-12 col-lg-6 p-0">
+                                    <img src="https://i.imgur.com/0x9rQuB.png" alt="restaurant template image" width="100%"/>
+                                </div>
+                                <div className="col-12 col-lg-6 order-sm-first text-center ">
                                     <a href="https://ar-webdesigns-restaurant.herokuapp.com/" target="_blank" className="project-title">Restaurant Template</a>
                                     <p>
-                                        Template for a restaurant website with customizable components
+                                        Template for a restaurant website with customizable components.<br/>
+                                        Built with Angular
                                     </p>
-                                    <div className="w-100">
-                                        <p className="project-stack">
-                                            Tech Stack:<br/>
-                                            React
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="col-6 p-0 text-left">
-                                    <img src="https://i.imgur.com/0x9rQuB.png" alt="test" width="100%"/>
                                 </div>
                             </div>
                         </div>
