dd72d6744a65ce781459ebc2970fba1b61fc1ecf
Alejandro Rios <Alx.Rios91@gmail.com>
2019-08-22 23:11:04 -0500

    intro animation test 2

--- src/components/pts/Collinear.js
+++ src/components/pts/Collinear.js
@@ -11,9 +11,9 @@ class Collinear extends PtsCanvas {
   _create() {
     // this.space.bindMouse().bindTouch().play();
     let r = this.space.size.minValue().value/2;
-    for(let x=0; x< 50; x++){
+    for(let x=0; x< 100; x++){
       let ln = new Group(Pt.make(2, r, true), Pt.make(2, -r, true));
-      ln.moveBy(this.space.center).rotate2D(x*Math.PI/50, this.space.center);
+      ln.moveBy(this.space.center).rotate2D(x*Math.PI/100, this.space.center);
       this.pairs.push(ln);
     }
   }
@@ -25,14 +25,14 @@ class Collinear extends PtsCanvas {
     for(let x=0, y=this.pairs.length; x<y; x++){
       let ln = this.pairs[x];
       ln.rotate2D(Const.one_degree/10, this.space.center);
-      let collinear = Line.collinear(ln[0], ln[1], this.space.pointer, 0.1);
-      if(collinear){
-        this.form.stroke("#fff").line(ln);
-      }else{
-        let side = Line.sideOfPt2D(ln, this.space.pointer);
-        this.form.stroke( (side<0) ? "rgba(0,135,199,.25)" : "rgba(240,114,12,.25)" ).line( ln );
-      }
-      this.form.fillOnly("rgba(255,255,255,0.8").points( ln, 0.5);
+      // let collinear = Line.collinear(ln[0], ln[1], this.space.pointer, 0.1);
+      // if(collinear){
+      //   this.form.stroke("#fff").line(ln);
+      // }else{
+        // }
+          let side = Line.sideOfPt2D(ln, this.space.pointer);
+          this.form.stroke( (side<0) ? "rgba(0,135,199,.25)" : "rgba(240,114,12,.25)" ).line( ln );
+        this.form.fillOnly("rgba(255,255,255,0.8").points( ln, 0.5);
     }
   }
   
